1433ede3870b5294b14d1687344cb8c60e251ff7
Rupam <rupamdas832@gmail.com>
2021-05-09 01:52:38 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import './App.css';
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <h1>📚Quizz App</h1>
+    </div>
+  );
+}
+
+export default App;
