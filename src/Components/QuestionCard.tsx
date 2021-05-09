cc03ed089acc611e05799465a33d19a5ac57ce35
Rupam <rupamdas832@gmail.com>
2021-05-10 00:58:11 +0530

    feat: reducer with typescript added

--- src/Components/QuestionCard.tsx
+++ src/Components/QuestionCard.tsx
@@ -1,19 +1,68 @@
-import React, { useState } from 'react'
+import React, { useReducer, useState } from 'react'
+import { Option, Question } from '../Data/quizOne.types'
 import quizOneData from "../Data/quizOneData"
+
 
 export const QuestionCard = () => {
 
-    const [questionNumber, setQuestionNumber] = useState(1)
+    type Action = 
+    | {type: "INCREASE_SCORE", payload: number } 
+    | {type: "DECREASE_SCORE", payload: number} 
+    | {type: "RESET"}
+    | {type: "NEXT_QUESTION"}
+    | {type: "PREVIOUS_QUESTION"}
 
-    const currentQuestion = quizOneData.questions[questionNumber - 1]
+    type StatusState = "starting" | "progress" | "end"
+
+    type InitialState = {
+        score: number;
+        status: StatusState,
+        questionNumber: number,
+        totalQuestions: number
+    }
+
+    const StoreReducer = (state: typeof initialState, action: Action) => {
+        switch (action.type) {
+            case "RESET":
+                return {...state, score: 0, questionNumber: 1}
+            case "INCREASE_SCORE":
+                return {...state, score: state.score + action.payload}
+            case "DECREASE_SCORE":
+                return {...state, score: state.score - action.payload}
+            case "NEXT_QUESTION":
+                return {...state, questionNumber: state.totalQuestions === state.questionNumber ? (state.totalQuestions) : (state.questionNumber + 1)}
+            case "PREVIOUS_QUESTION":
+                return {...state, questionNumber: state.questionNumber <= 1 ? (1) : (state.questionNumber - 1)}
+            default:
+                return state;
+        }
+    }
+
+    const initialState: InitialState = {
+        score: 0,
+        status: "starting",
+        questionNumber: 1,
+        totalQuestions: quizOneData.questions.length
+    }
+
+    const [storeState, storeDispatch] = useReducer(StoreReducer,initialState)
+
+    const checkAnswer = (currentQuestion: Question, option : Option) => {
+        return option.isCorrect ? storeDispatch({type: "INCREASE_SCORE", payload: currentQuestion.points}) : storeDispatch({type: "DECREASE_SCORE", payload: currentQuestion.negativePoints})
+    }
+
+    const currentQuestion = quizOneData.questions[storeState.questionNumber - 1]
 
     return (
         <div>
-            <button onClick={() => setQuestionNumber(questionNumber => questionNumber + 1)}>Inc</button>
-            <p><span>{questionNumber} : </span> {currentQuestion.question}</p>
+            <h1>Score: {storeState.score}</h1>
+            <button onClick={() => storeDispatch({type: "RESET"})}>Reset</button>
+            <p><span>{storeState.questionNumber} : </span> {currentQuestion.question}</p>
             {currentQuestion.options.map((option,idx) => {
-                return <button>{option.value}</button>
+                return <button onClick={() => checkAnswer(currentQuestion, option)}>{option.value}</button>
             })}
+            <button onClick={() => storeDispatch({type: "PREVIOUS_QUESTION"})}>⬅️</button>
+            <button onClick={() => storeDispatch({type: "NEXT_QUESTION"})}>➡️</button>
         </div>
     )
 }
